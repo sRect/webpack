d425990cec7faf7dac43611b4537703056d6cef8
srect <fcqun@sina.cn>
2018-06-26 15:12:43 +0800

    :construction: optimization配置

--- webpack-config/webpack.prod.js
+++ webpack-config/webpack.prod.js
@@ -9,7 +9,13 @@ const prod = {
   optimization: {
     splitChunks: {
       cacheGroups: {
-        vendor: {
+        commons: {  // 抽离自己写的公共代码
+          chunks: "initial",
+          name: "common", // 打包后的文件名，任意命名
+          minChunks: 2,//最小引用2次
+          minSize: 0 // 只要超出0字节就生成一个新包
+        },
+        vendor: {  // 抽离第三方插件
           test: /node_modules/, // 指定是node_modules下的第三方包
           chunks: 'initial',
           minChunks: 2,

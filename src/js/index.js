3d10f0210b8ff580b57696927d51b5e0531fbacb
Fang Chaoqun <fcqun@sina.cn>
2019-03-27 13:14:00 +0800

    :sparkles: feat: 使用identicons生成哈希头像

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@
 import $ from 'jquery'
 import moment from 'moment'
 import "../css/index"
+import Identicon from 'identicon.js';
+import createHash from 'create-hash';
 
 
 if (module.hot) {
@@ -26,3 +28,21 @@ async(true).then(data => {
 })
 
 $("#test").css({ "color": 'red' }).html(moment().format('YYYY-MM-DD HH:mm:ss'))
+
+// 头像随机生成
+
+let hash = createHash('md5');
+hash.update('sRect');
+let result = hash.digest('hex');
+
+let options = {
+  foreground: [0, 152, 255, 255],               // rgba black
+  background: [240, 240, 240, 255],         // rgba white
+  margin: 0.1,                              // 20% margin
+  size: 200,                                // 420px square
+  format: 'png'                             // use SVG instead of PNG
+};
+
+let imgData = new Identicon(result, options).toString();
+let imgUrl = 'data:image/png;base64,' + imgData; // 这就是头像的base64码
+$("#avatar")[0].src = imgUrl;
